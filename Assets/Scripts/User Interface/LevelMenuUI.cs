2ee555aeda6365c3df937741b444bdedc7091121
Master00021 <107310643+Master00021@users.noreply.github.com>
2024-07-14 13:15:32 -0400

    feat (UI): added level stats functionality

---
+++ Assets/Scripts/User Interface/LevelMenuUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+namespace Game {
+
+    internal sealed class LevelMenuUI : MonoBehaviour {
+        
+        [SerializeField] private LevelData _levelData;
+        [SerializeField] private TextMeshProUGUI _percentage;
+        [SerializeField] private TextMeshProUGUI _completed;
+        [SerializeField] private GameObject[] _shells;
+
+        private void OnEnable() {
+            PercentageObtained();
+            ShellCollected();
+            Completed();
+        }
+
+        private void PercentageObtained() {
+            if (_levelData.Percentage >= 99.0f) {
+                Completed();
+            }
+            
+            _percentage.text = _levelData.Percentage.ToString("0") + "%";
+        }
+
+        private void ShellCollected() {
+            if (_levelData.FirstShell) {
+                _shells[0].SetActive(true);
+            }
+
+            if (_levelData.SecondShell) {
+                _shells[1].SetActive(true);
+            }
+
+            if (_levelData.ThirdShell) {
+                _shells[2].SetActive(true);
+            }
+        }
+
+        private void Completed() {
+            if (_levelData.Completed) {
+                _percentage.gameObject.SetActive(false);
+                _completed.gameObject.SetActive(true);
+
+
+            }
+        }
+
+    }
+}
